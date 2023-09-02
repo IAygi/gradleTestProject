991e6bf2a4694b95d943049cd01991885c0d681d
Krack Digger <grave1kro@gmail.com>
2023-09-02 19:59:28 +0300

    removing conflicting code

--- src/test/java/ru/iaygi/api/tests/ApiTest.java
+++ src/test/java/ru/iaygi/api/tests/ApiTest.java
@@ -40,7 +40,6 @@ public class ApiTest extends TestBase {
     @DisplayName("Создание пользователя")
     @Description("Проверить корректное создание пользователя")
     void createUser() {
-
         step("Создать пользователя", () -> {
             restAssured.createUser(user).shouldHave(statusCode(201));
         });
@@ -61,7 +60,6 @@ public class ApiTest extends TestBase {
     @DisplayName("Получение списка пользователей")
     @Description("Проверить корректное получение списка пользователей")
     void getAllUsers() {
-
         step("Получить список пользователей", () -> {
             var res = restAssured.getAllUsers().shouldHave(statusCode(200)).getResponseAsList(UsersDTO.class);
             assertThat(res).extracting("login", "name", "surname")
@@ -74,7 +72,6 @@ public class ApiTest extends TestBase {
     @DisplayName("Получение пользователя по login")
     @Description("Проверить корректное получение пользователя по login")
     void getUser() {
-
         step("Получить пользователя admin", () -> {
             var res = restAssured.getUser("admin").shouldHave(statusCode(200)).getResponseAs(UsersDTO.class);
             assertAll(
@@ -92,15 +89,25 @@ public class ApiTest extends TestBase {
     }
 
     @Test
+    @DisplayName("Получение несуществующего пользователя по login")
+    @Description("Проверить, что при получении несуществующего пользователя по login ответ 404")
+    void getNotRealUser() {
+        String login = userRandom().login() + "_not_real";
+
+        step("Получить пользователя по login", () -> {
+            restAssured.getUser(login).shouldHave(statusCode(404));
+        });
+    }
+
+    @Test
     @DisplayName("Обновление имени пользователя по login")
     @Description("Проверить корректное обновление имени пользователя по login")
     void updateUserNameByLogin() {
+        String name = userRandom().name();
 
         step("Создать пользователя", () -> {
             restAssured.createUser(user);
         });
-
-        String name = userRandom().name();
 
         step("Обновить имя пользователя по login", () -> {
             UpdateUserDTO updateUserDTO = new UpdateUserDTO()
@@ -116,7 +123,7 @@ public class ApiTest extends TestBase {
                     .contains(tuple(user.login(), name));
         });
 
-        step("удалить пользователя по login", () -> {
+        step("Удалить пользователя по login", () -> {
             restAssured.deleteUser(user.login()).shouldHave(statusCode(204));
         });
     }
@@ -125,12 +132,11 @@ public class ApiTest extends TestBase {
     @DisplayName("Обновление фамилии пользователя по login")
     @Description("Проверить корректное бновление фамилии пользователя по login")
     void updateUserSurnameByLogin() {
+        String surname = userRandom().surname();
 
         step("Создать пользователя", () -> {
             restAssured.createUser(user);
         });
-
-        String surname = userRandom().surname();
 
         step("Обновить фамилию пользователя по login", () -> {
             UpdateUserDTO updateUserDTO = new UpdateUserDTO()
@@ -154,8 +160,7 @@ public class ApiTest extends TestBase {
     @Test
     @DisplayName("Удаление пользователя по login")
     @Description("Проверить корректное удаление пользователя по login")
-    void deleteUserByLogin() {
-
+    void deleteNotRealUserByLogin() {
         step("Создать пользователя", () -> {
             restAssured.createUser(user);
             var res = restAssured.getAllUsers().shouldHave(statusCode(200)).getResponseAsList(UsersDTO.class);
@@ -171,6 +176,17 @@ public class ApiTest extends TestBase {
             var res = restAssured.getAllUsers().shouldHave(statusCode(200)).getResponseAsList(UsersDTO.class);
             assertThat(res).extracting("login", "name")
                     .doesNotContain(tuple(user.login(), user.name()));
+        });
+    }
+
+    @Test
+    @DisplayName("Удаление несуществующего пользователя по login")
+    @Description("Проверить, что при удалении несуществующего пользователя по login ответ 404")
+    void deleteUserByLogin() {
+        String login = userRandom().login() + "_not_real";
+
+        step("Удалить пользователя по login", () -> {
+            restAssured.deleteUser(login).shouldHave(statusCode(404));
         });
     }
 }
